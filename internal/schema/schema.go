69c98def6887dcb38713511b4325e2c57aeeae43
Luis Eduardo <eduardo.devop@gmail.com>
2025-03-08 04:33:46 +0000

    Implement JSON marshaling and unmarshaling for PrimitiveType and ProcedureType

--- internal/schema/schema.go
+++ internal/schema/schema.go
@@ -5,20 +5,44 @@ import (
 	"github.com/orsinium-labs/enum"
 )
 
-type (
-	// PrimitiveType represents the allowed primitive data types
-	PrimitiveType enum.Member[string]
+// PrimitiveType represents the allowed primitive data types
+type PrimitiveType enum.Member[string]
 
-	// ProcedureType represents the allowed procedure types
-	ProcedureType enum.Member[string]
-)
+func (primitiveType PrimitiveType) MarshalJSON() ([]byte, error) {
+	return []byte(`"` + primitiveType.Value + `"`), nil
+}
 
+func (primitiveType *PrimitiveType) UnmarshalJSON(data []byte) error {
+	value := string(data[1 : len(data)-1])
+	*primitiveType = PrimitiveType{value}
+	return nil
+}
+
+// Allowed primitive types
 var (
 	PrimitiveTypeString  = PrimitiveType{"string"}
 	PrimitiveTypeInt     = PrimitiveType{"int"}
 	PrimitiveTypeFloat   = PrimitiveType{"float"}
 	PrimitiveTypeBoolean = PrimitiveType{"boolean"}
+)
 
+// ProcedureType represents the allowed procedure types
+type ProcedureType enum.Member[string]
+
+// MarshalJSON implements the json.Marshaler interface
+func (procedureType ProcedureType) MarshalJSON() ([]byte, error) {
+	return []byte(`"` + procedureType.Value + `"`), nil
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface
+func (procedureType *ProcedureType) UnmarshalJSON(data []byte) error {
+	value := string(data[1 : len(data)-1])
+	*procedureType = ProcedureType{value}
+	return nil
+}
+
+// Allowed procedure types
+var (
 	ProcedureTypeQuery    = ProcedureType{"query"}
 	ProcedureTypeMutation = ProcedureType{"mutation"}
 )
