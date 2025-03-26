b67224dbffb05008681e93e6cbd6a81b1354ba7d
Luis Eduardo <eduardo.devop@gmail.com>
2025-03-26 05:23:49 +0000

    Refactor AST node structures in URPC by removing Position field from various types

--- internal/urpc/ast/ast.go
+++ internal/urpc/ast/ast.go
@@ -19,72 +19,55 @@ const (
 	NodeTypeTypeReference
 )
 
-type Position struct {
-	FileName string
-	Line     int
-	Column   int
-}
-
 type Node interface {
 	NodeType() NodeType
-	GetPosition() Position
 }
 
 // Root node
 type Schema struct {
-	Position   Position
 	Version    Version
 	Types      []TypeDeclaration
 	Procedures []ProcDeclaration
 }
 
-func (s *Schema) NodeType() NodeType    { return NodeTypeSchema }
-func (s *Schema) GetPosition() Position { return s.Position }
+func (s *Schema) NodeType() NodeType { return NodeTypeSchema }
 
 // Version
 type Version struct {
-	Position Position
-	Value    int
+	IsSet bool
+	Value int
 }
 
-func (v *Version) NodeType() NodeType    { return NodeTypeVersion }
-func (v *Version) GetPosition() Position { return v.Position }
+func (v *Version) NodeType() NodeType { return NodeTypeVersion }
 
 // Type system
 type TypeDeclaration struct {
-	Position Position
-	Name     string
-	Doc      string
-	Fields   []Field
+	Name   string
+	Doc    string
+	Fields []Field
 }
 
-func (t *TypeDeclaration) NodeType() NodeType    { return NodeTypeTypeDeclaration }
-func (t *TypeDeclaration) GetPosition() Position { return t.Position }
+func (t *TypeDeclaration) NodeType() NodeType { return NodeTypeTypeDeclaration }
 
 type Field struct {
-	Position    Position
 	Name        string
 	Type        Type
 	Optional    bool
 	Validations []ValidationRule
 }
 
-func (f *Field) NodeType() NodeType    { return NodeTypeField }
-func (f *Field) GetPosition() Position { return f.Position }
+func (f *Field) NodeType() NodeType { return NodeTypeField }
 
 type ValidationRule struct {
-	Position Position
 	Name     string
 	Params   []any
 	ErrorMsg string
 }
 
-func (v *ValidationRule) NodeType() NodeType    { return NodeTypeValidationRule }
-func (v *ValidationRule) GetPosition() Position { return v.Position }
+func (v *ValidationRule) NodeType() NodeType { return NodeTypeValidationRule }
 
 // Procedures
 type ProcDeclaration struct {
-	Position Position
 	Name     string
 	Doc      string
 	Input    Input
@@ -92,38 +75,30 @@ type ProcDeclaration struct {
 	Metadata Metadata
 }
 
-func (p *ProcDeclaration) NodeType() NodeType    { return NodeTypeProcDeclaration }
-func (p *ProcDeclaration) GetPosition() Position { return p.Position }
+func (p *ProcDeclaration) NodeType() NodeType { return NodeTypeProcDeclaration }
 
 type Input struct {
-	Position Position
-	Fields   []Field
+	Fields []Field
 }
 
-func (i *Input) NodeType() NodeType    { return NodeTypeInput }
-func (i *Input) GetPosition() Position { return i.Position }
+func (i *Input) NodeType() NodeType { return NodeTypeInput }
 
 type Output struct {
-	Position Position
-	Fields   []Field
+	Fields []Field
 }
 
-func (o *Output) NodeType() NodeType    { return NodeTypeOutput }
-func (o *Output) GetPosition() Position { return o.Position }
+func (o *Output) NodeType() NodeType { return NodeTypeOutput }
 
 // Metadata
 type Metadata struct {
-	Position Position
-	Entries  []KeyValue
+	Entries []KeyValue
 }
 
-func (m *Metadata) NodeType() NodeType    { return NodeTypeMetadata }
-func (m *Metadata) GetPosition() Position { return m.Position }
+func (m *Metadata) NodeType() NodeType { return NodeTypeMetadata }
 
 type KeyValue struct {
-	Position Position
-	Key      string
-	Value    any // string|number|boolean
+	Key   string
+	Value any // string|number|boolean
 }
 
 // Type system implementations
@@ -133,37 +108,29 @@ type Type interface {
 }
 
 type PrimitiveType struct {
-	Position Position
-	Name     string // "string", "int", etc.
+	Name string // "string", "int", etc.
 }
 
-func (p *PrimitiveType) NodeType() NodeType    { return NodeTypePrimitiveType }
-func (p *PrimitiveType) GetPosition() Position { return p.Position }
-func (p *PrimitiveType) TypeName() string      { return p.Name }
+func (p *PrimitiveType) NodeType() NodeType { return NodeTypePrimitiveType }
+func (p *PrimitiveType) TypeName() string   { return p.Name }
 
 type ArrayType struct {
-	Position Position
-	Element  Type
+	Element Type
 }
 
-func (a *ArrayType) NodeType() NodeType    { return NodeTypeArrayType }
-func (a *ArrayType) GetPosition() Position { return a.Position }
-func (a *ArrayType) TypeName() string      { return "array" }
+func (a *ArrayType) NodeType() NodeType { return NodeTypeArrayType }
+func (a *ArrayType) TypeName() string   { return "array" }
 
 type InlineObjectType struct {
-	Position Position
-	Fields   []Field
+	Fields []Field
 }
 
-func (o *InlineObjectType) NodeType() NodeType    { return NodeTypeInlineObjectType }
-func (o *InlineObjectType) GetPosition() Position { return o.Position }
-func (o *InlineObjectType) TypeName() string      { return "object" }
+func (o *InlineObjectType) NodeType() NodeType { return NodeTypeInlineObjectType }
+func (o *InlineObjectType) TypeName() string   { return "object" }
 
 type TypeReference struct {
-	Position Position
-	Name     string
+	Name string
 }
 
-func (t *TypeReference) NodeType() NodeType    { return NodeTypeTypeReference }
-func (t *TypeReference) GetPosition() Position { return t.Position }
-func (t *TypeReference) TypeName() string      { return t.Name }
+func (t *TypeReference) NodeType() NodeType { return NodeTypeTypeReference }
+func (t *TypeReference) TypeName() string   { return t.Name }
