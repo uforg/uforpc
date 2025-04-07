d3e3ab7f231e9078a9977a6ba2b53611b30dbeec
Luis Eduardo <eduardo.devop@gmail.com>
2025-04-07 19:11:11 +0000

    Add initial URPC schema and main command implementation

---
+++ cmd/urpc/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/alexflint/go-arg"
+	"github.com/uforg/uforpc/internal/urpc/lsp"
+)
+
+//go:embed init-schema.urpc
+var initSchema []byte
+
+type allArgs struct {
+	Init *initCmd `arg:"subcommand:init" help:"Initialize a new URPC schema in the specified path"`
+	LSP  *lspCmd  `arg:"subcommand:lsp" help:"Start the LSP server"`
+}
+
+type initCmd struct {
+	Path string `arg:"positional" help:"The file or directory path to initialize the URPC schema in, if no file name is provided, the file will be named 'schema.urpc'"`
+}
+
+type lspCmd struct{}
+
+func main() {
+	var args allArgs
+	arg.MustParse(&args)
+
+	if args.Init != nil {
+		initCmdFn(args.Init)
+	}
+
+	if args.LSP != nil {
+		lspCmdFn(args.LSP)
+	}
+}
+
+func initCmdFn(args *initCmd) {
+	if args.Path == "" || args.Path == "." {
+		args.Path = "./schema.urpc"
+	}
+
+	if !strings.HasSuffix(args.Path, ".urpc") {
+		args.Path = path.Join(args.Path, "schema.urpc")
+	}
+
+	if err := os.WriteFile(args.Path, initSchema, 0644); err != nil {
+		log.Fatalf("failed to write init schema: %s", err)
+	}
+
+	fmt.Printf("URPC schema initialized in %s\n", args.Path)
+}
+
+func lspCmdFn(_ *lspCmd) {
+	lspInstance := lsp.New(os.Stdin, os.Stdout)
+	if err := lspInstance.Run(); err != nil {
+		log.Fatalf("failed to run lsp: %s", err)
+	}
+}
