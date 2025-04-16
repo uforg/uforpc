9d5f2c3cb3789b0e39fd185b19c22a1e0133108e
Luis Eduardo <eduardo.devop@gmail.com>
2025-04-16 06:24:03 +0000

    Refactor comments in ToJSON function for clarity and consistency

--- internal/transpile/to_json.go
+++ internal/transpile/to_json.go
@@ -8,9 +8,13 @@ import (
 // ToJSON transpiles an UFO-RPC AST schema to it's JSON representation.
 //
 // The imports, extends and external docstrings of the AST Schema are expected
-// to be already resolved, so if there are any unresolved imports or extends, the
-// transpiler will ignore them (imports and extends) or threat them as
-// literal (docstrings).
+// to be already resolved.
+//
+// If there are any unresolved imports or extends, the transpiler
+// will ignore them.
+//
+// If there are any unresolved external docstrings, the transpiler will
+// treat them literally as strings.
 //
 // All comments and comment blocks will be ignored.
 func ToJSON(astSchema ast.Schema) (schema.Schema, error) {
