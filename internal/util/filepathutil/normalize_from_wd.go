4b2aba33eb2032d8cbbf4464675d1eda1326b8d2
Luis Eduardo <eduardo.devop@gmail.com>
2025-04-18 22:32:00 +0000

    Add NormalizeFromWD function to convert relative paths to absolute paths

--- internal/util/filepathutil/normalize_from_wd.go
+++ internal/util/filepathutil/normalize_from_wd.go
@@ -2,20 +2,25 @@ package filepathutil
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
 )
 
-// ToAbsolutePath converts a relative path to an absolute path
+// NormalizeFromWD converts a relative path to a canonical absolute path
 // using the current working directory as the base.
 //
 // If relativePath is already absolute, it is returned as is.
-func ToAbsolutePath(relativePath string) (string, error) {
+func NormalizeFromWD(relativePath string) (string, error) {
 	// Get working dir
-	wd, err := filepath.Abs(".")
+	wd, err := os.Getwd()
 	if err != nil {
 		return relativePath, fmt.Errorf("failed to get working directory: %w", err)
 	}
 
+	// Required because Normalize requires a file path and
+	// not a directory path
+	dummyFile := filepath.Join(wd, "dummy.txt")
+
 	// Join and normalize the working dir and the relative path
-	return Normalize(wd, relativePath)
+	return Normalize(dummyFile, relativePath)
 }
