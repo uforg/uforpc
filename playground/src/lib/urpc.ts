8b0a4ccc5b25be6bbecf2a1bc538e0cf2634b55a
Luis Eduardo <eduardo.devop@gmail.com>
2025-04-22 19:24:34 +0000

    Add WASM initialization and loading UI to the playground layout

---
+++ playground/src/lib/urpc.ts
@@ -0,0 +1,96 @@
+// deno-lint-ignore-file no-explicit-any
+
+/**
+ * Docs: https://go.dev/wiki/WebAssembly
+ */
+
+/**
+ * Load a script asynchronously
+ *
+ * @param src The script source
+ * @returns A promise that resolves when the script has been loaded
+ */
+function loadScript(src: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const s = document.createElement("script");
+    s.src = src;
+    s.onload = () => resolve();
+    s.onerror = () => reject(new Error(`failed to load ${src}`));
+    document.head.appendChild(s);
+  });
+}
+
+/**
+ * Check if the wasm has been initialized
+ *
+ * @returns True if the wasm has been initialized, false otherwise
+ */
+function isInitialized(): boolean {
+  return (window as any).__urpcWasmReady;
+}
+
+/**
+ * Wait until the wasm has been initialized
+ *
+ * @returns A promise that resolves when the wasm has been initialized
+ */
+function waitUntilInitialized(): Promise<void> {
+  if (isInitialized()) return Promise.resolve();
+
+  return new Promise((resolve) => {
+    const interval = setInterval(() => {
+      if (isInitialized()) {
+        clearInterval(interval);
+        resolve();
+      }
+    }, 100);
+  });
+}
+
+/**
+ * Initialize the wasm
+ *
+ * @returns A promise that resolves when the wasm has been initialized
+ */
+async function initWasm(): Promise<void> {
+  const execURL = "./urpc/wasm_exec.js";
+  const wasmURL = "./urpc/urpc.wasm";
+
+  if (isInitialized()) return;
+  await loadScript(execURL);
+
+  const go = new (window as any).Go();
+  const { instance } = await WebAssembly.instantiateStreaming(
+    await fetch(wasmURL),
+    go.importObject,
+  );
+  go.run(instance);
+
+  (window as any).__urpcWasmReady = true;
+}
+
+/**
+ * Format an URPC schema
+ *
+ * @param input The URPC schema to format
+ * @returns The formatted URPC schema
+ */
+async function cmdFmt(input: string): Promise<string> {
+  await waitUntilInitialized();
+  return (window as any).cmdFmt(input);
+}
+
+/**
+ * Transpile an URPC schema to JSON and vice versa based on the original
+ * extension
+ *
+ * @param sourceExt The original extension of the file (.json or .urpc)
+ * @param input The schema to transpile
+ * @returns The transpiled schema
+ */
+async function cmdTranspile(sourceExt: string, input: string): Promise<string> {
+  await waitUntilInitialized();
+  return (window as any).cmdTranspile(sourceExt, input);
+}
+
+export { cmdFmt, cmdTranspile, initWasm, isInitialized, waitUntilInitialized };
--- playground/src/routes/+layout.svelte
+++ playground/src/routes/+layout.svelte
@@ -1,6 +1,37 @@
 <script lang="ts">
   let { children } = $props();
   import "../app.css";
+  import { onMount } from "svelte";
+  import { fade } from "svelte/transition";
+  import { Loader } from "@lucide/svelte";
+  import { initWasm, waitUntilInitialized } from "$lib/urpc";
+
+  let wasmInitialized = $state(false);
+
+  onMount(async () => {
+    await initWasm();
+    await waitUntilInitialized();
+    wasmInitialized = true;
+  });
 </script>
 
-{@render children()}
+{#if !wasmInitialized}
+  <main
+    out:fade={{ duration: 200 }}
+    class="flex flex-col fixed top-0 left-0 h-screen w-screen items-center justify-center"
+  >
+    <img src="/assets/logo-square.png" alt="UFO RPC Logo" class="size-[150px]">
+    <h1 class="text-3xl font-bold mb-2">UFO RPC Playground</h1>
+    <h2 class="mb-6">Loading WASM binary...</h2>
+    <Loader class="animate animate-spin size-10" />
+  </main>
+{/if}
+
+{#if wasmInitialized}
+  <main
+    transition:fade={{ duration: 200 }}
+    class="w-[100dvw] h-[100dvh]"
+  >
+    {@render children()}
+  </main>
+{/if}
