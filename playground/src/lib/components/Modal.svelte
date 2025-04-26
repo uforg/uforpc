41578c883efcd8fbd1ae914872d8511707df228d
Luis Eduardo <eduardo.devop@gmail.com>
2025-04-26 06:31:37 +0000

    Refactor Modal component to use svelte-portal for rendering, improving modal display and accessibility.

--- playground/src/lib/components/Modal.svelte
+++ playground/src/lib/components/Modal.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { fade } from "svelte/transition";
+  import Portal from "svelte-portal";
   import { mergeClasses } from "$lib/helpers/mergeClasses";
   import type { ClassValue } from "$lib/helpers/mergeClasses";
 
@@ -37,30 +38,32 @@
 </script>
 
 {#if isOpen}
-  <div
-    class="z-40 w-screen h-screen fixed top-0 left-0"
-    transition:fade={{ duration: 200 }}
-  >
-    <button
-      class={mergeClasses(
-        "w-full h-full z-10 bg-black/30",
-        backdropClassName,
-      )}
-      onclick={backdropClose ? closeModal : undefined}
-      aria-label="Close modal"
-    >
-    </button>
-
+  <Portal target="body">
     <div
-      class={mergeClasses(
-        "absolute z-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2",
-        "bg-base-100 rounded-box p-4 w-[90dvw] max-w-lg max-h-[90dvh] shadow-xl",
-        className,
-      )}
+      class="z-40 w-screen h-screen fixed top-0 left-0"
+      transition:fade={{ duration: 200 }}
     >
-      {#if children}
-        {@render children()}
-      {/if}
+      <button
+        class={mergeClasses(
+          "w-full h-full z-10 bg-black/30",
+          backdropClassName,
+        )}
+        onclick={backdropClose ? closeModal : undefined}
+        aria-label="Close modal"
+      >
+      </button>
+
+      <div
+        class={mergeClasses(
+          "absolute z-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2",
+          "bg-base-100 rounded-box p-4 w-[90dvw] max-w-lg max-h-[90dvh] shadow-xl",
+          className,
+        )}
+      >
+        {#if children}
+          {@render children()}
+        {/if}
+      </div>
     </div>
-  </div>
+  </Portal>
 {/if}
