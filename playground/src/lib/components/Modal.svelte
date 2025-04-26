a820d8de403ac05437ea74c2f0150d2dec17176a
Luis Eduardo <eduardo.devop@gmail.com>
2025-04-26 03:46:52 +0000

    Add Modal component with customizable backdrop and transition effects for improved user interaction

---
+++ playground/src/lib/components/Modal.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+  import { mergeClasses } from "$lib/helpers/mergeClasses";
+  import type { ClassValue } from "$lib/helpers/mergeClasses";
+
+  interface Props {
+    children?: any;
+    isOpen?: boolean;
+    class?: ClassValue;
+    backdropClass?: ClassValue;
+    backdropClose?: boolean;
+  }
+
+  let {
+    children,
+    isOpen = $bindable(false),
+    class: className,
+    backdropClass: backdropClassName,
+    backdropClose = true,
+  }: Props = $props();
+
+  const closeModal = () => (isOpen = false);
+</script>
+
+{#if isOpen}
+  <div
+    class="z-40 w-screen h-screen fixed top-0 left-0"
+    transition:fade={{ duration: 200 }}
+  >
+    <button
+      class={mergeClasses(
+        "w-full h-full z-10 bg-black/30",
+        backdropClassName,
+      )}
+      onclick={backdropClose ? closeModal : undefined}
+      aria-label="Close modal"
+    >
+    </button>
+
+    <div
+      class={mergeClasses(
+        "absolute z-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2",
+        "bg-base-100 rounded-box p-4 w-[90dvw] max-w-lg max-h-[90dvh] shadow-xl",
+        className,
+      )}
+    >
+      {#if children}
+        {@render children()}
+      {/if}
+    </div>
+  </div>
+{/if}
