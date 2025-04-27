8bab245e614138a47f4a73a5ffd2b9979d167eeb
Luis Eduardo <eduardo.devop@gmail.com>
2025-04-27 05:20:43 +0000

    Add smooth scrolling to hash anchors on page load using onMount lifecycle method for improved navigation experience.

--- playground/src/routes/+page.svelte
+++ playground/src/routes/+page.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
   import { isscrolledAction } from "$lib/actions/isScrolled.svelte";
+  import { onMount } from "svelte";
   import Aside from "./components/Aside.svelte";
   import Header from "./components/Header.svelte";
   import Main from "./components/Main.svelte";
 
   let isScrolled = $state(false);
+
+  // if has hash anchor navigate to it
+  onMount(() => {
+    if (window.location.hash) {
+      const element = document.getElementById(
+        window.location.hash.slice(1),
+      );
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  });
 </script>
 
 <div class="w-[100dvw] h-[100dvh] flex justify-start">
