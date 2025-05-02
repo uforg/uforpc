10a57f14954fa46706f672b16159a8a9b9087cf6
Luis Eduardo <eduardo.devop@gmail.com>
2025-05-02 17:10:13 +0000

    Add toggle functionality in Query.svelte for better user interaction. Integrate fade transition and Chevron icons to enhance the visual experience when expanding and collapsing input fields.

--- playground/src/routes/components/MainItemQuery/Query.svelte
+++ playground/src/routes/components/MainItemQuery/Query.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+  import { fade } from "svelte/transition";
+  import { ChevronDown, ChevronRight, ChevronUp } from "@lucide/svelte";
   import type { ProcedureDefinitionNode } from "$lib/urpcTypes";
+  import H2 from "$lib/components/H2.svelte";
   import Field from "./Field.svelte";
   import Snippets from "./Snippets.svelte";
 
@@ -10,13 +13,41 @@
   const { proc }: Props = $props();
 
   let value = $state({ root: {} });
+  let isOpen = $state(true);
+  const toggleIsOpen = () => (isOpen = !isOpen);
 </script>
 
 {#if proc.input}
   <div class="flex space-x-4">
-    <div class="space-y-2 flex-grow">
-      <Field fields={proc.input} path="root" bind:value />
+    <div class="flex-grow">
+      <button
+        class={[
+          "btn justify-start border-base-content/20 rounded-box w-full",
+          {
+            "rounded-b-none": isOpen,
+          },
+        ]}
+        onclick={toggleIsOpen}
+      >
+        {#if isOpen}
+          <ChevronDown class="size-5" />
+        {/if}
+        {#if !isOpen}
+          <ChevronRight class="size-5" />
+        {/if}
+        <span class="ml-2">Try it out</span>
+      </button>
+      {#if isOpen}
+        <div
+          class="p-4 rounded-box rounded-t-none border border-t-0 border-base-content/20 space-y-2"
+          transition:fade={{ duration: 100 }}
+        >
+          <H2>Input</H2>
+          <Field fields={proc.input} path="root" bind:value />
+        </div>
+      {/if}
     </div>
+
     <Snippets {value} />
   </div>
 {/if}
