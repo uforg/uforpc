b30b27a5b297f8184f93e7222023e5e90e7a712a
Luis Eduardo <eduardo.devop@gmail.com>
2025-05-03 03:22:57 +0000

    Add hover styles to Fieldset component for improved interactivity and visual feedback.

--- playground/src/routes/components/MainItemQuery/Fieldset.svelte
+++ playground/src/routes/components/MainItemQuery/Fieldset.svelte
@@ -8,7 +8,9 @@
 
 <fieldset
   class={[
+    "fieldset-component",
     "fieldset border border-base-content/20 rounded-box w-full p-4 space-y-2",
+    "hover:border-base-content/50 has-[.fieldset-component:hover]:hover:border-base-content/20",
   ]}
 >
   {@render children()}
