2e36468453275a1021d5296524d0f8043a3982fb
Luis Eduardo <eduardo.devop@gmail.com>
2025-05-04 23:33:17 +0000

    Add support for additional programming languages in SnippetsCode component for curl command conversion. Enhance language grouping and organization, improving usability and accessibility of code snippets.

--- playground/src/routes/components/MainItemQuery/SnippetsCode.svelte
+++ playground/src/routes/components/MainItemQuery/SnippetsCode.svelte
@@ -27,6 +27,89 @@
       label: "Curl",
       func: (code: string) => code,
     },
+
+    //////////////////////////////////////
+    // Alphabetically ordered from here //
+    //////////////////////////////////////
+
+    {
+      group: "Ansible",
+      langCode: "yaml",
+      label: "Ansible",
+      func: curlconverter.toAnsible,
+    },
+    {
+      group: "C",
+      langCode: "c",
+      label: "C",
+      func: curlconverter.toC,
+    },
+    {
+      group: "C#",
+      langCode: "csharp",
+      label: "C#",
+      func: curlconverter.toCSharp,
+    },
+    {
+      group: "Clojure",
+      langCode: "clojure",
+      label: "Clojure",
+      func: curlconverter.toClojure,
+    },
+    {
+      group: "Dart",
+      langCode: "dart",
+      label: "Dart",
+      func: curlconverter.toDart,
+    },
+    {
+      group: "Elixir",
+      langCode: "elixir",
+      label: "Elixir",
+      func: curlconverter.toElixir,
+    },
+    {
+      group: "Go",
+      langCode: "go",
+      label: "Go",
+      func: curlconverter.toGo,
+    },
+    {
+      group: "HTTP",
+      langCode: "http",
+      label: "HTTP",
+      func: curlconverter.toHTTP,
+    },
+    {
+      group: "HTTPie",
+      langCode: "httpie",
+      label: "HTTPie",
+      func: curlconverter.toHttpie,
+    },
+    {
+      group: "Java",
+      langCode: "java",
+      label: "Java HttpClient",
+      func: curlconverter.toJava,
+    },
+    {
+      group: "Java",
+      langCode: "java",
+      label: "Java HttpUrlConnection",
+      func: curlconverter.toJavaHttpUrlConnection,
+    },
+    {
+      group: "Java",
+      langCode: "java",
+      label: "Java Jsoup",
+      func: curlconverter.toJavaJsoup,
+    },
+    {
+      group: "Java",
+      langCode: "java",
+      label: "Java OkHttp",
+      func: curlconverter.toJavaOkHttp,
+    },
     {
       group: "JavaScript",
       langCode: "javascript",
@@ -46,6 +129,96 @@
       func: curlconverter.toJavaScriptXHR,
     },
     {
+      group: "Julia",
+      langCode: "julia",
+      label: "Julia",
+      func: curlconverter.toJulia,
+    },
+    {
+      group: "JSON",
+      langCode: "json",
+      label: "JSON",
+      func: curlconverter.toJsonString,
+    },
+    {
+      group: "Kotlin",
+      langCode: "kotlin",
+      label: "Kotlin",
+      func: curlconverter.toKotlin,
+    },
+    {
+      group: "Lua",
+      langCode: "lua",
+      label: "Lua",
+      func: curlconverter.toLua,
+    },
+    {
+      group: "MATLAB",
+      langCode: "matlab",
+      label: "MATLAB",
+      func: curlconverter.toMATLAB,
+    },
+    {
+      group: "Node.js",
+      langCode: "javascript",
+      label: "Node.js Axios",
+      func: curlconverter.toNodeAxios,
+    },
+    {
+      group: "Node.js",
+      langCode: "javascript",
+      label: "Node.js Got",
+      func: curlconverter.toNodeGot,
+    },
+    {
+      group: "Node.js",
+      langCode: "javascript",
+      label: "Node.js Ky",
+      func: curlconverter.toNodeKy,
+    },
+    {
+      group: "Node.js",
+      langCode: "javascript",
+      label: "Node.js node-fetch",
+      func: curlconverter.toNodeFetch,
+    },
+    {
+      group: "Node.js",
+      langCode: "javascript",
+      label: "Node.js request",
+      func: curlconverter.toNodeRequest,
+    },
+    {
+      group: "Node.js",
+      langCode: "javascript",
+      label: "Node.js SuperAgent",
+      func: curlconverter.toNodeSuperAgent,
+    },
+    {
+      group: "Node.js",
+      langCode: "javascript",
+      label: "Node.js http",
+      func: curlconverter.toNodeHttp,
+    },
+    {
+      group: "Objective-C",
+      langCode: "objective-c",
+      label: "Objective-C",
+      func: curlconverter.toObjectiveC,
+    },
+    {
+      group: "OCaml",
+      langCode: "ocaml",
+      label: "OCaml",
+      func: curlconverter.toOCaml,
+    },
+    {
+      group: "Perl",
+      langCode: "perl",
+      label: "Perl",
+      func: curlconverter.toPerl,
+    },
+    {
       group: "PHP",
       langCode: "php",
       label: "PHP Curl",
@@ -56,6 +229,66 @@
       langCode: "php",
       label: "PHP Guzzle",
       func: curlconverter.toPhpGuzzle,
+    },
+    {
+      group: "PowerShell",
+      langCode: "powershell",
+      label: "PowerShell",
+      func: curlconverter.toPowershellWebRequest,
+    },
+    {
+      group: "Python",
+      langCode: "python",
+      label: "Python Requests",
+      func: curlconverter.toPython,
+    },
+    {
+      group: "Python",
+      langCode: "python",
+      label: "Python http.client",
+      func: curlconverter.toPythonHttp,
+    },
+    {
+      group: "R",
+      langCode: "r",
+      label: "R httr",
+      func: curlconverter.toR,
+    },
+    {
+      group: "R",
+      langCode: "r",
+      label: "R httr2",
+      func: curlconverter.toRHttr2,
+    },
+    {
+      group: "Ruby",
+      langCode: "ruby",
+      label: "Ruby Net::HTTP",
+      func: curlconverter.toRuby,
+    },
+    {
+      group: "Ruby",
+      langCode: "ruby",
+      label: "Ruby HTTParty",
+      func: curlconverter.toRubyHttparty,
+    },
+    {
+      group: "Rust",
+      langCode: "rust",
+      label: "Rust",
+      func: curlconverter.toRust,
+    },
+    {
+      group: "Swift",
+      langCode: "swift",
+      label: "Swift",
+      func: curlconverter.toSwift,
+    },
+    {
+      group: "Wget",
+      langCode: "bash",
+      label: "Wget",
+      func: curlconverter.toWget,
     },
   ];
 
