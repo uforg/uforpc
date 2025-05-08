fd9aafa1dc0e0d36e5972f7e946e3a578ffd01b4
Luis Eduardo <eduardo.devop@gmail.com>
2025-05-08 04:37:49 +0000

    Update copywasm.sh and package.json to reference the correct paths for urpc.wasm and schema.json, ensuring proper file copying and schema generation.

--- playground/copywasm.sh
+++ playground/copywasm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp ../dist/urpc.wasm ./static/urpc/urpc.wasm && \
-cp ../dist/wasm_exec.js ./static/urpc/wasm_exec.js && \
+cp ../urpc/dist/urpc.wasm ./static/urpc/urpc.wasm && \
+cp ../urpc/dist/wasm_exec.js ./static/urpc/wasm_exec.js && \
 cp ./node_modules/web-tree-sitter/tree-sitter.wasm ./static/tree-sitter.wasm && \
 cp ./node_modules/curlconverter/dist/tree-sitter-bash.wasm ./static/tree-sitter-bash.wasm
