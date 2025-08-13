b9ee4fb15a06205c7b75a7503bd7578c91e7701b
Luis Eduardo <eduardo.devop@gmail.com>
2025-08-13 23:33:45 +0000

    Update linting rules in core.dart: Added 'unused_field' to the ignore_for_file directive to suppress warnings for unused fields, enhancing code clarity and maintainability.

--- urpc/internal/codegen/dart/pieces/core.dart
+++ urpc/internal/codegen/dart/pieces/core.dart
@@ -14,7 +14,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-// ignore_for_file: unused_element
+// ignore_for_file: unused_element, unused_field
 
 import 'dart:async';
 import 'dart:convert' as convert;
