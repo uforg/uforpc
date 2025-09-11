b768b7d688d7412766f865a9c5e072dafa56a8f9
Luis Eduardo <eduardo.devop@gmail.com>
2025-09-11 19:19:48 +0000

    Add version management file: Define version constants for release process

---
+++ playground/src/lib/version.ts
@@ -0,0 +1,6 @@
+/**
+ * version is replaced during the release process by the latest Git tag
+ * and should not be manually edited.
+ */
+export const version = "0.0.0-dev";
+export const versionWithPrefix = `v${version}`;
