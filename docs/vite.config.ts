babfdc3edf2fe3e76ee311760ddda8d3719fb524
Luis Eduardo <eduardo.devop@gmail.com>
2025-09-15 18:14:07 +0000

    Add gendocs script

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,7 +1,18 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { run } from "vite-plugin-run";
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [
+    tailwindcss(),
+    sveltekit(),
+    run([
+      {
+        name: "gendocs",
+        run: ["npm", "run", "gendocs"],
+        pattern: ["src/docs/**/*.md", "src/docs/**/*.svx"],
+      },
+    ]),
+  ],
 });
