7682e9e4ca34c6678213aca36f490a6f01d44c57
Luis Eduardo <eduardo.devop@gmail.com>
2025-09-15 19:12:28 +0000

    feat: restructure layout and page components for improved header and content display

--- docs/src/routes/+layout.svelte
+++ docs/src/routes/+layout.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
+  import Logo from "$lib/components/Logo.svelte";
+
   import "../app.css";
 
   let { children } = $props();
 </script>
+
+<header
+  class="flex w-full items-center justify-between bg-base-300 p-4 desk:px-12"
+>
+  <a href="/">
+    <Logo class="h-[30px] w-auto" />
+  </a>
+  <nav>
+    <a href="/docs" class="link">Docs</a>
+  </nav>
+</header>
 
 <main class="h-[100dvh] w-[100dvw] overflow-auto">
   {@render children?.()}
--- docs/src/routes/+page.svelte
+++ docs/src/routes/+page.svelte
@@ -1,5 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-  documentation
-</p>
+<script lang="ts">
+  import H1 from "$lib/components/H1.svelte";
+  import H2 from "$lib/components/H2.svelte";
+  import UfoAbduction from "$lib/components/UfoAbduction.svelte";
+</script>
+
+<header class="grid w-full grid-cols-1 gap-4 py-8 desk:grid-cols-12">
+  <div
+    class="flex items-center justify-center desk:col-span-7 desk:justify-end"
+  >
+    <hgroup>
+      <H1>UFO RPC</H1>
+      <H2>The RPC framework that puts developer experience first.</H2>
+    </hgroup>
+  </div>
+
+  <div
+    class="flex items-center justify-center desk:col-span-5 desk:justify-start"
+  >
+    <div>
+      <UfoAbduction width={200} />
+    </div>
+  </div>
+</header>
