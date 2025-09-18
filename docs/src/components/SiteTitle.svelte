bb8fc6dc9183b817b8ec1ef505a6a665749ea70a
Luis Eduardo <eduardo.devop@gmail.com>
2025-09-18 02:14:28 +0000

    feat: wrap Logo component in a link and add reset styles for links

--- docs/src/components/SiteTitle.svelte
+++ docs/src/components/SiteTitle.svelte
@@ -7,9 +7,29 @@
   import Logo from "./Logo.svelte";
 </script>
 
-<Logo class="custom-logo" />
+<a href="/" class="flex items-center justify-center">
+  <Logo class="custom-logo" />
+</a>
 
 <style>
+  /* Reset default link styles */
+  a,
+  a:hover,
+  a:focus {
+    text-decoration: none;
+    background: transparent;
+    color: inherit;
+    border: none;
+    outline: none;
+  }
+
+  /* Add focus style */
+  a:focus-visible {
+    box-shadow: 0 0 0 2px var(--sl-color-gray-2);
+    border-radius: 2px;
+  }
+
+  /* Same size as the original */
   :global(.custom-logo) {
     height: calc(var(--sl-nav-height) - 2 * var(--sl-nav-pad-y));
     width: auto;
