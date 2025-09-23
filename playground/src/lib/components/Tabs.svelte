bf743be1ebde7aa33d974e2537ddc3c5b50af865
Luis Eduardo <eduardo.devop@gmail.com>
2025-09-23 06:19:18 +0000

    refactor: enhance Tabs component by adding tooltip support and improving click handling logic

--- playground/src/lib/components/Tabs.svelte
+++ playground/src/lib/components/Tabs.svelte
@@ -4,10 +4,14 @@
 
   import { type ClassValue, mergeClasses } from "$lib/helpers/mergeClasses";
 
+  import Tooltip from "./Tooltip.svelte";
+
   export interface TabItem<T extends string = string> {
     id: T;
     label?: string;
     icon?: Component<IconProps, {}, "">;
+    action?: () => void;
+    tooltipText?: string;
   }
 
   interface Props<T extends string> {
@@ -38,6 +42,17 @@
     active = id;
     if (onSelect) onSelect(id);
   };
+
+  const handleClick = (id: T) => {
+    const tab = items.find((t) => t.id === id);
+
+    if (tab?.action) {
+      tab.action();
+      return;
+    }
+
+    handleSelect(id);
+  };
 </script>
 
 <div
@@ -47,17 +62,7 @@
   )}
 >
   {#each items as tab}
-    <button
-      class={[
-        "btn join-item border-base-content/20 flex-grow",
-        buttonClass,
-        active === tab.id && "btn-primary",
-        active === tab.id && activeButtonClass,
-        active !== tab.id && inactiveButtonClass,
-      ]}
-      onclick={() => handleSelect(tab.id)}
-      type="button"
-    >
+    {#snippet buttonContent()}
       {#if tab.icon}
         <tab.icon class={mergeClasses("size-4", iconClass)} />
       {/if}
@@ -66,6 +71,30 @@
           {tab.label}
         </span>
       {/if}
-    </button>
+    {/snippet}
+
+    {#snippet button()}
+      <button
+        class={[
+          "btn join-item border-base-content/20 flex-grow",
+          buttonClass,
+          active === tab.id && "btn-primary",
+          active === tab.id && activeButtonClass,
+          active !== tab.id && inactiveButtonClass,
+        ]}
+        onclick={() => handleClick(tab.id)}
+        type="button"
+      >
+        {@render buttonContent()}
+      </button>
+    {/snippet}
+
+    {#if tab.tooltipText}
+      <Tooltip content={tab.tooltipText} placement="top">
+        {@render button()}
+      </Tooltip>
+    {:else}
+      {@render button()}
+    {/if}
   {/each}
 </div>
